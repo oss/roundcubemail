6040b6531bb3fc49bf99741399c23218bd683f42
KATIE POSKAITIS <katiepru@nbcs.rutgers.edu>
2012-05-21 17:17:22 -0400

    fixed jqueryui pointer in webtools.php

--- plugins/webtools/webtools.php
+++ plugins/webtools/webtools.php
@@ -63,14 +63,16 @@ class webtools extends rcube_plugin
       if (!file_exists($this->home."/skins/$skin/webtools.css"))
 	  $skin = 'default';
       
-      $rutgers_skin = '../../skins/rutgers';      
+      $rutgers_skin = '../../skins/rutgers';
+
+	  $jqueryui = '../jqueryui/js';
 
       // add style for taskbar button (must be here) and Help UI    
       $this->include_stylesheet("skins/$skin/webtools.css");
       $this->include_stylesheet("$rutgers_skin/jquery-ui-1.7.2.custom.css");
       $this->include_stylesheet("$rutgers_skin/ui.checkbox.css");
       $this->include_stylesheet("$rutgers_skin/ui.selectmenu.css");
-      $this->include_script("$rutgers_skin/jquery-ui-1.7.2.custom.min.js");
+      $this->include_script("$jqueryui/jquery-ui-1.8.18.custom.min.js");
       $this->include_script("$rutgers_skin/ui.checkbox.js");
       $this->include_script("$rutgers_skin/ui.selectmenu.js");
       $this->include_script("js/webtools.js");
