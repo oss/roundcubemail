492ad95cb0eda8bd0145029686b470569ec7a927
jgr68 <john,ryan@rutgers.edu>
2014-06-19 13:29:15 -0400

    forwarding is now working

--- plugins/webtools/webtools/forward/functions.js
+++ plugins/webtools/webtools/forward/functions.js
@@ -1,187 +1,178 @@
-function forward_add ()
-{
-	if (validateEmail())
-	{
-		var email = $('#email').attr('value');
-		var state = $('#localcopy-check').attr('checked');
-		rcmail.http_post('plugin.webtoolsforward-post', { 'func':'add', 'value':email, 'check':state });
-	}
-	else
-	{
-		$.pnotify({
-			pnotify_title: 'Invalid',
-			pnotify_text: 'You must enter a valid email address.',
-			pnotify_type: 'error',
-			pnotify_animation: 'slide',
-			pnotify_height: '100px'
-
-		});
-	}
+function forward_add() {
+  	var email = document.getElementById("email").value;
+  	var localcopy_check = document.getElementById("localcopy-check").value;
+	// next two lines for debugging only... remove when done
+	console.log("email: "+email);
+	console.log("localcopy_check: "+localcopy_check);
+    if (validateEmail()) {
+        var a = $("#email").attr("value");
+        var b = $("#localcopy-check").attr("checked");
+		/*
+        rcmail.http_post("plugin.webtoolsforward-post", {
+            func: "add",
+            value: a,
+            check: b
+        })
+		*/
+        rcmail.http_post("plugin.webtoolsforward-post", {
+            func: "add",
+            value: email,
+            check: localcopy_check
+        })
+    } else {
+        $.pnotify({
+            pnotify_title: "Invalid",
+            pnotify_text: "You must enter a valid email address.",
+            pnotify_type: "error",
+            pnotify_animation: "slide",
+            pnotify_height: "100px"
+        })
+    }
 }
 
-function forward_remove_all ()
-{
-	rcmail.http_post('plugin.webtoolsforward-post', { 'func':'delall' });
+function forward_remove_all() {
+    rcmail.http_post("plugin.webtoolsforward-post", {
+        func: "delall"
+    })
 }
 
-function forward_remove (email)
-{
-	rcmail.http_post('plugin.webtoolsforward-post', { 'func':'del', 'value':email });
+function forward_remove(a) {
+    rcmail.http_post("plugin.webtoolsforward-post", {
+        func: "del",
+        value: a
+    })
 }
 
-function forward_chtype ()
-{
-	var state = $('#localcopy-check').attr('checked');
-	rcmail.http_post('plugin.webtoolsforward-post', { 'func':'chtype', 'value':state });
+function forward_chtype() {
+    var a = $("#localcopy-check").attr("checked");
+    rcmail.http_post("plugin.webtoolsforward-post", {
+        func: "chtype",
+        value: a
+    })
 }
 
-function delall_dialog()
-{
-	$('#dialog-test').dialog('open');
+function delall_dialog() {
+    $("#dialog-test").dialog("open")
 }
 
-function forward_post_callback (data)
-{
-	if (data.func == 'add')
-	{
-		if (data.alreadyexists == 'true')
-		{
-			//var stack_bottomright = {"dir1":"up", "dir2":"left", "firstpos1":15, "firstpos2":15};
-			$.pnotify({
-				pnotify_title: 'Failed',
-				pnotify_text: 'You\'re already forwarding to that address!',
-				pnotify_type: 'error',
-				pnotify_animation: 'slide',
-				pnotify_height: '100px'
-			});
-			return;
-		}
-		if (data.youremail == 'true')
-		{
-			$.pnotify({
-				pnotify_title: 'Failed',
-				pnotify_text: 'You can\'t forward to your own address!',
-				pnotify_type: 'error',
-				pnotify_animation: 'slide',
-				pnotify_height: '100px'
-			});
-			return;
-		}
-
-		if (data.no_mxr == 'true')
-		{   
-			$.pnotify({
-				pnotify_title: 'Failed',
-				pnotify_text: 'Mail server doesn\'t seem to exist!',
-				pnotify_type: 'error',
-				pnotify_animation: 'slide',
-				pnotify_height: '100px'
-			});
-			return;
-		}
-
-		var rowstyle;
-		var last = $('#localcopy-row');
-		if (last.prev().attr('class') == "rowstyle0") rowstyle = 'rowstyle1';
-		else rowstyle = 'rowstyle0';
-		last.before("<tr style='display:none' class=\""+rowstyle+"\" id=\""+data.id+"\"><td>" +
-				"<a href=\"#\" onclick=\"forward_remove('"+data.value+"')\">" +
-				"<img src=\"plugins/webtools/webtools/forward/delete.png\" />" +
-				"</a></td><td><p class=\"emailstyle\">"+data.value+"</p></td></tr>");
-		$('#'+data.id).fadeIn();
-		$('#email').attr('value','');
-		num_forwards++;
-	}
-	else if (data.func =='del')
-	{
-		$('#'+data.id).fadeOut();
-		num_forwards--;
-	}
-	else if (data.func == 'chtype')
-	{
-		$.pnotify({
-			pnotify_title: 'Success',
-			pnotify_notice_icon: 'ui-icon ui-icon-check',
-			pnotify_text: 'Setting saved!',
-			//        pnotify_type: 'success',
-			pnotify_animation: 'slide',
-			pnotify_height: '100px'
-		});
-	}
-	else if (data.func == 'delall')
-	{
-		$('#forward-table tr').each( function(index) {
-			if ((this.getAttribute('class') == 'rowstyle0') || (this.getAttribute('class') == 'rowstyle1')) {
-				$(this).fadeOut();
-			}
-		});
-		$.pnotify({
-			pnotify_title: 'Success',
-			pnotify_notice_icon: 'ui-icon ui-icon-check',
-			pnotify_text: 'Your forward(s) have been removed successfully.',
-			//      pnotify_type: 'success',
-			pnotify_animation: 'slide',
-			pnotify_height: '100px'
-		});
-		num_forwards = 0;
-	}
-	if (num_forwards == 0)
-	{
-		$('#removeall').hide();
-		$('label[for=localcopy-check],#localcopy-check').hide();
-		//$('#localcopy-check').attr('disabled', true);
-		$('#localcopy-check').attr('checked', false);
-	}
-	else
-	{
-		$('#removeall').show();
-		$('label[for=localcopy-check],#localcopy-check').show();
-		//$('#localcopy-check').removeAttr('disabled');
-	}
+function forward_post_callback(c) {
+    if (c.func == "add") {
+        if (c.alreadyexists == "true") {
+            $.pnotify({
+                pnotify_title: "Failed",
+                pnotify_text: "You're already forwarding to that address!",
+                pnotify_type: "error",
+                pnotify_animation: "slide",
+                pnotify_height: "100px"
+            });
+            return
+        }
+        if (c.youremail == "true") {
+            $.pnotify({
+                pnotify_title: "Failed",
+                pnotify_text: "You can't forward to your own address!",
+                pnotify_type: "error",
+                pnotify_animation: "slide",
+                pnotify_height: "100px"
+            });
+            return
+        }
+        if (c.no_mxr == "true") {
+            $.pnotify({
+                pnotify_title: "Failed",
+                pnotify_text: "Mail server doesn't seem to exist!",
+                pnotify_type: "error",
+                pnotify_animation: "slide",
+                pnotify_height: "100px"
+            });
+            return
+        }
+        var b;
+        var a = $("#localcopy-row");
+        if (a.prev().attr("class") == "rowstyle0") {
+            b = "rowstyle1"
+        } else {
+            b = "rowstyle0"
+        }
+        a.before("<tr style='display:none' class=\"" + b + '" id="' + c.id + '"><td><a href="#" onclick="forward_remove(\'' + c.value + '\')"><img src="plugins/webtools/webtools/forward/delete.png" /></a></td><td><p class="emailstyle">' + c.value + "</p></td></tr>");
+        $("#" + c.id).fadeIn();
+        $("#email").attr("value", "");
+        num_forwards++
+    } else {
+        if (c.func == "del") {
+            $("#" + c.id).fadeOut();
+            num_forwards--
+        } else {
+            if (c.func == "chtype") {
+                $.pnotify({
+                    pnotify_title: "Success",
+                    pnotify_notice_icon: "ui-icon ui-icon-check",
+                    pnotify_text: "Setting saved!",
+                    pnotify_animation: "slide",
+                    pnotify_height: "100px"
+                })
+            } else {
+                if (c.func == "delall") {
+                    $("#forward-table tr").each(function(d) {
+                        if ((this.getAttribute("class") == "rowstyle0") || (this.getAttribute("class") == "rowstyle1")) {
+                            $(this).fadeOut()
+                        }
+                    });
+                    $.pnotify({
+                        pnotify_title: "Success",
+                        pnotify_notice_icon: "ui-icon ui-icon-check",
+                        pnotify_text: "Your forward(s) have been removed successfully.",
+                        pnotify_animation: "slide",
+                        pnotify_height: "100px"
+                    });
+                    num_forwards = 0
+                }
+            }
+        }
+    } if (num_forwards == 0) {
+        $("#removeall").hide();
+        $("label[for=localcopy-check],#localcopy-check").hide();
+        $("#localcopy-check").attr("checked", false)
+    } else {
+        $("#removeall").show();
+        $("label[for=localcopy-check],#localcopy-check").show()
+    }
 }
 
-function validateEmail(){
-	var email = $('#email');
-	var a = email.val();
-	var filter = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(?:[a-zA-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)$/;
-	if(filter.test(a)) {
-		email.removeClass('ui-state-error');
-		return true;
-	} else {
-		email.addClass('ui-state-error');
-		return false;
-	}
+function validateEmail() {
+    var c = $("#email");
+    var b = c.val();
+    var d = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(?:[a-zA-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)$/;
+    if (d.test(b)) {
+        c.removeClass("ui-state-error");
+        return true
+    } else {
+        c.addClass("ui-state-error");
+        return false
+    }
 }
-
-rcmail.addEventListener ('plugin.forward-post-callback', forward_post_callback);
-$(document).ready( function() {
-	$("#email").bind("keyup",function(e) {
-		validateEmail();
-		var key = e.keyCode || e.which;
-		if (key == 13) {
-			forward_add();
-		}
-		});
-
-	$("#email").change(validateEmail);
-
-	$('#dialog-test').dialog (
-	{  
-		modal:true, 
-		resizable:false, 
-		autoOpen:false,
-		buttons: 
-		{   
-			'Delete All' : function()
-			{
-				forward_remove_all ();
-				$(this).dialog('close');
-			},  
-			'Cancel':function()
-			{  
-				$(this).dialog('close'); 
-			}   
-		}   
-	}); 
+rcmail.addEventListener("plugin.forward-post-callback", forward_post_callback);
+$(document).ready(function() {
+    $("#email").bind("keyup", function(b) {
+        validateEmail();
+        var a = b.keyCode || b.which;
+        if (a == 13) {
+            forward_add()
+        }
+    });
+    $("#email").change(validateEmail);
+    $("#dialog-test").dialog({
+        modal: true,
+        resizable: false,
+        autoOpen: false,
+        buttons: {
+            "Delete All": function() {
+                forward_remove_all();
+                $(this).dialog("close")
+            },
+            Cancel: function() {
+                $(this).dialog("close")
+            }
+        }
+    })
 });
-
